010bec34b1a689a9261a7ab3066f5bc600d7b22e
VivekBhat39 <vivek.bhat571@gmail.com>
2025-02-12 16:50:32 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Users from './components/Users';
 import Courses from './components/Courses';
-import Demo from './components/Demo';
+import CourseAssigner from './components/CourseAssigner';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Demo />} />
+          <Route path="/" element={<CourseAssigner />} />
           <Route path="/users" element={<Users />} />
           <Route path="/courses" element={<Courses />} />
         </Routes>
